774ac42fddacdd3c02d97e64cfffec329774e4b8
Edrick Vince Sinsuan <sedrickvince@gmail.com>
2020-11-16 21:26:55 +0300

    sched/tune: Remove top-app boost
    Signed-off-by: ederekun <sedrickvince@gmail.com>

--- kernel/sched/tune.c
+++ kernel/sched/tune.c
@@ -895,7 +895,7 @@ static void write_default_values(struct cgroup_subsys_state *css)
 		{ "background",	0, 0, 0, 0 },
 		{ "foreground",	0, 0, 0, 0 },
 		{ "rt",		0, 0, 0, 0 },
-		{ "top-app",	10, 0, 0, 0 },
+		{ "top-app",	0, 0, 0, 0 },
 	};
 	int i;
 
