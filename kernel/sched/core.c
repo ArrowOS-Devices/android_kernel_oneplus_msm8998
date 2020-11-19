69aca43494c3fe8548d676ebadc4c5ee75def5f5
Edrick Vince Sinsuan <sedrickvince@gmail.com>
2020-11-19 15:51:21 +0300

    sched/core: Add offset to make sure that the time remains accurate
    Let's make sure that time during timekeeping_suspended is considered to have accurate readings regarding the task's runtime. Let scheduler know that timer had been suspended when the timekeeping resumes and we return entity tick back to hrtimer.
    
    Signed-off-by: ederekun <sedrickvince@gmail.com>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -579,7 +579,7 @@ static void init_rq_hrtick(struct rq *rq)
 	rq->hrtick_csd.info = rq;
 #endif
 
-	hrtimer_init(&rq->hrtick_timer, CLOCK_MONOTONIC, HRTIMER_MODE_REL);
+	hrtimer_init(&rq->hrtick_timer, CLOCK_BOOTTIME, HRTIMER_MODE_REL);
 	rq->hrtick_timer.function = hrtick;
 }
 #else	/* CONFIG_SCHED_HRTICK */
