d75fb7f479a999b02153ab974918c841d801b6c2
Edrick Vince Sinsuan <sedrickvince@gmail.com>
2020-11-19 23:40:51 +0300

    sched/fair: Trigger iowait boost only if the task is top-app or foreground
    Related article:
    https://lkml.org/lkml/2020/1/23/985
    
    Although this method is not inline with what's being discussed in the article (due to how old the kernel version is), this similarly addresses the said issue in the message regarding iowait boost being triggered by background tasks in iowait. Thus, with this commit we have added a function which checks and compares if the task's cgroup is top-app or foreground before triggering the cpufreq iowait update.
    We have not used task_is_boosted because due to our dynamic stune setup, the top-app will not have consistent schedtune boost.
    
    With this change, there are now less reasons to not enable iowait boost as this greatly reduces the amount of power consumption caused by this feature. Thus, let's remove the switch to remove unnecessary overhead.
    
    Signed-off-by: ederekun <sedrickvince@gmail.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -5051,6 +5051,14 @@ static unsigned long cpu_util_without(int cpu, struct task_struct *p);
 static inline unsigned long cpu_util_freq(int cpu);
 
 /*
+ * Check if task belongs to *st_name cgroup.
+ */
+static inline bool task_belongs_to_cgroup(struct task_struct *p, char *st_name)
+{
+	return !strcmp(task_css(p, schedtune_cgrp_id)->cgroup->kn->name, st_name);
+}
+
+/*
  * The enqueue_task method is called before nr_running is
  * increased. Here we update the fair scheduling stats and
  * then put the task into the rbtree:
@@ -5094,8 +5102,12 @@ enqueue_task_fair(struct rq *rq, struct task_struct *p, int flags)
 	 * If in_iowait is set, the code below may not trigger any cpufreq
 	 * utilization updates, so do it here explicitly with the IOWAIT flag
 	 * passed.
+	 * 
+	 * We only trigger the iowait boost if only the task belongs to either
+	 * top-app or foreground cgroup.
 	 */
-	if (p->in_iowait)
+	if (p->in_iowait && (task_belongs_to_cgroup(p, "top-app") || 
+		task_belongs_to_cgroup(p, "foreground")))
 		cpufreq_update_util(rq, SCHED_CPUFREQ_IOWAIT);
 
 	for_each_sched_entity(se) {
